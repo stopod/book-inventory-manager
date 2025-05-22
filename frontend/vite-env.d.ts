d4a5b1636e259786b514db354fbb52e20f4ddedd
stopod <gjkv8@icloud.com>
2025-05-22 21:51:32 +0900

    update vite-env.d.ts

--- frontend/vite-env.d.ts
+++ frontend/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+  // 他の環境変数もここに追加するわよ～
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
